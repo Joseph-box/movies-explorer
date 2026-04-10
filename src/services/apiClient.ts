7c532acbe49286244489982be2b0efdb6f89d2e8
Joseph Azagoury <joseph.azagoury@gmail.com>
2026-04-10 14:40:01 +0200

    added a public read only access token

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,10 +1,14 @@
 import axios, { type AxiosRequestConfig } from "axios";
 
+const TMDB_TOKEN =
+  import.meta.env.VITE_TMDB_ACCESS_TOKEN ||
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzQ1MDE2OWQ4YWI2NGUwMDQ0MzIxYTE2YWQ1NWZjMSIsIm5iZiI6MTc1NDMwMzA3MC43NjEsInN1YiI6IjY4OTA4YTVlMTU4NGJmNmM5ZjYxZjRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rwKLy8iTDSZn6NkXlya5He1MiQWJ-XbI9QMOz64VLNw";
+
 const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + import.meta.env.VITE_TMDB_ACESS_TOKEN,
+    Authorization: `Bearer ${TMDB_TOKEN}`,
   },
 });
 
